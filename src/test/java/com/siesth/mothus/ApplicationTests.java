944a59424f997ac56d3b13549af5178e011dd474
Samlach2222 <lachaudsamuel@protonmail.com>
2023-11-12 22:46:15 +0100

    Add initial Java Spring project

---
+++ src/test/java/com/siesth/mothus/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siesth.mothus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
