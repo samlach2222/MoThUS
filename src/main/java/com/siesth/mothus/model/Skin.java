c3a6304365bd541b2e160cd1759ff9df49a38ca5
tdegieux@gmail.com <tdegieux@gmail.com>
2023-11-21 11:53:11 +0100

    Init for skin and skininventory

--- src/main/java/com/siesth/mothus/model/Skin.java
+++ src/main/java/com/siesth/mothus/model/Skin.java
@@ -1,4 +1,11 @@
 package com.siesth.mothus.model;
 
 public class Skin {
+
+    int idSkin;
+    SkinType type;
+
+    SkinRarity rarity;
+
+    String cssFile;
 }
--- src/main/java/com/siesth/mothus/model/SkinInventory.java
+++ src/main/java/com/siesth/mothus/model/SkinInventory.java
@@ -1,4 +1,9 @@
 package com.siesth.mothus.model;
 
+import jakarta.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+
 public class SkinInventory {
+
+
 }
