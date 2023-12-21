6c53c99858104e431102bdb60a9a3e1fd2cd88c0
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2023-12-21 19:52:38 +0100

    Get/Create today game and change game date to localDate (time 00:00:00)

--- src/main/java/com/siesth/mothus/repository/GameRepository.java
+++ src/main/java/com/siesth/mothus/repository/GameRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game, Integer> {
-    // You can add custom query methods if needed
-
-    //findTopByOrderByDateOfTheGameDesc
+public interface GameRepository extends JpaRepository<Game, Long> {
     Game findTopByOrderByDateOfTheGameDesc();
 }
