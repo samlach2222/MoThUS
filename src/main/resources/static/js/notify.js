04c7f32f7e0d3b3454fb146d15afc927af41a68b
Samlach2222 <lachaudsamuel@protonmail.com>
2023-12-26 18:37:54 +0100

    Add Toastify for toast notifications instead of alert()

---
+++ src/main/resources/static/js/notify.js
@@ -0,0 +1,29 @@
+function notifyError(message) {
+    Toastify({
+        text: message,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "#b90022",
+        }
+    }).showToast();
+}
+
+function notifySuccess(message) {
+    Toastify({
+        text: message,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+    }).showToast();
+}
