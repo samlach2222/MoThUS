1ba9b789804afbea18e4941f8728e78aa7bba96a
Samlach2222 <lachaudsamuel@protonmail.com>
2023-12-26 16:59:14 +0100

    Login now compare encoded passwords

--- src/main/java/com/siesth/mothus/repository/UserRepository.java
+++ src/main/java/com/siesth/mothus/repository/UserRepository.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsUserByUsernameAndPassword(String username, String password);
+
+    User findUserByUsername(String username);
     boolean existsUserByUsername(String username);
     boolean existsUserByMail(String mail);
 }
