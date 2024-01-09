22a77bf6de0c1b611f8f4f859e1521b81e3254b7
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2024-01-09 20:34:03 +0100

    Redirect to /login on error (page not found etc.)

---
+++ src/main/java/com/siesth/mothus/controller/ManageError.java
@@ -0,0 +1,28 @@
+package com.siesth.mothus.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ManageError implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        int statusCode = Integer.parseInt(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE).toString());
+
+        switch (statusCode) {
+            // Not Found
+            case 404:
+                // Redirect to /login when a page doesn't exist
+                // It will then redirect to /playZone if the user is already logged in
+                System.err.println("Error code " + statusCode + ": Page not found");
+                return "redirect:/login";
+            default:
+                System.err.println("Error code " + statusCode);
+                return "redirect:/login";
+        }
+    }
+}
