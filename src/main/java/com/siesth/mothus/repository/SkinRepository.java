ed96b53caf4eccce5ce34b64637009dcefdc5ca1
Samlach2222 <lachaudsamuel@protonmail.com>
2024-01-11 15:53:41 +0100

    Execute skins init when launching server

--- src/main/java/com/siesth/mothus/repository/SkinRepository.java
+++ src/main/java/com/siesth/mothus/repository/SkinRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SkinRepository extends JpaRepository<Skin, Long> {
-    Skin findSkinBySkinName(String skinName);
-    boolean existsSkinBySkinName(String skinName);
+
 }
